9ffc76767e1579eda8ecc517993e1d12c4f1c2c3
mateog03 <104777599+mateog03@users.noreply.github.com>
2024-05-26 13:26:17 +0000

    Peter Alert (real)

--- src/main/java/it/univr/wordautomata/controller/Components.java
+++ src/main/java/it/univr/wordautomata/controller/Components.java
@@ -2,7 +2,15 @@ package it.univr.wordautomata.controller;
 
 import com.brunomnsilva.smartgraph.containers.ContentZoomScrollPane;
 
+import it.univr.wordautomata.Main;
+import it.univr.wordautomata.alerts.Alerts;
+import it.univr.wordautomata.utils.Constants;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 /*
@@ -33,47 +41,71 @@ public class Components {
         this.stage = stage;
     }
 
-    public void setScene(Scene scene){
-        this.scene = scene;
+    private static int p = 0;
+    private static String peter = "PETER";
+
+    void peterAlert(KeyEvent e) {
+        char c = e.getCode().getChar().charAt(0);
+
+        if (peter.charAt(p) != c) {
+            p = c == 'P' ? 1 : 0;
+        } else {
+            p++;
+            if (p == peter.length()) {
+                Alert peterAlert = Alerts.createAlert(null, scene, "Peter Alert", "", "", ButtonType.OK);
+                peterAlert.setGraphic(new ImageView(new Image(Main.class
+                        .getResourceAsStream(
+                                Constants.ICON_BASE_FOLDER + "Petergriffin" + Constants.ICON_EXTENSION),
+                        100, 100, true, true)));
+                peterAlert.initOwner(stage.getOwner());
+                peterAlert.showAndWait();
+                p = 0;
+            }
+        }
     }
 
-    public void setMainPanel(MainPanel mainPanel){
+    public void setScene(Scene scene) {
+        this.scene = scene;
+        scene.setOnKeyPressed(this::peterAlert);
+    }
+
+    public void setMainPanel(MainPanel mainPanel) {
         this.mainPanel = mainPanel;
     }
 
-    public void setBottomBar(BottomBar bottomBar){
+    public void setBottomBar(BottomBar bottomBar) {
         this.bottomBar = bottomBar;
     }
 
-    public void setGraphPanel(GraphPanel graphPanel){
+    public void setGraphPanel(GraphPanel graphPanel) {
         this.graphPanel = graphPanel;
     }
 
-    public void setContentZoomScrollPane(ContentZoomScrollPane contentZoomScrollPane){
+    public void setContentZoomScrollPane(ContentZoomScrollPane contentZoomScrollPane) {
         this.contentZoomScrollPane = contentZoomScrollPane;
     }
 
-    public Stage getStage(){
+    public Stage getStage() {
         return stage;
     }
 
-    public Scene getScene(){
+    public Scene getScene() {
         return scene;
     }
 
-    public MainPanel getMainPanel(){
+    public MainPanel getMainPanel() {
         return mainPanel;
     }
 
-    public BottomBar getBottomBar(){
+    public BottomBar getBottomBar() {
         return bottomBar;
     }
 
-    public GraphPanel getGraphPanel(){
+    public GraphPanel getGraphPanel() {
         return graphPanel;
     }
 
-    public ContentZoomScrollPane getContentZoomScrollPane(){
+    public ContentZoomScrollPane getContentZoomScrollPane() {
         return contentZoomScrollPane;
     }
 }
